5c68b858d66a86a12b3c0231df267093b69a63dd
jpowell121 <jpowell@joelpowell.com>
2018-12-05 21:39:36 -0500

    adding initial component and removing boilerplate

---
+++ src/Components/UserManager.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class UserManager extends Component {
+  render() {
+    return (
+      <div>UserManager</div>
+    )
+  }
+}
+
+export default UserManager
